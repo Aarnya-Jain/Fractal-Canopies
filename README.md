645b947119a2fccf8b24e93d5c3488832de9be41
Aarnya Jain <aarnyajain417@gmail.com>
2026-02-27 01:49:10 +0530

    Added Basic Function to visualise Hardcoded values

---
+++ README.md
@@ -0,0 +1,23 @@
+# Fractal Canopies Visualiser
+
+Version - 1.0
+
+**Demo Images :**
+<center>
+
+- LEFT ANGLE + 20
+- RIGHT ANGLE - 20
+
+<img width="955" height="689" alt="Image" src="https://github.com/user-attachments/assets/19c19747-61d9-40c9-b509-fb2147d183e8" />
+
+- LEFT ANGLE + 20
+- RIGHT ANGLE - 33
+
+<img width="955" height="689" alt="Image" src="https://github.com/user-attachments/assets/77a225ce-7fee-4bc9-bfcf-1964fb631d11" />
+</center>
+
+
+**Compile using :**
+```C
+gcc ft.c -o ft -lraylib -lm -lpthread -ldl -lrt -lX11
+```
--- ft.c
+++ ft.c
@@ -1,1 +1,44 @@
 #include <raylib.h>
+#include <math.h>
+#include <stdio.h>
+
+#define WIDTH 950
+#define HEIGHT 650
+#define BRANCH_ANGLE 20
+
+void DrawTree(double leftAngle , double rightAngle, double ratio) {
+
+}
+
+void DrawBranch(Vector2 start,float length,float angle,float thickness) {
+  if(length < 1) return;
+  float x_end = start.x + sinf(angle)*length;
+  float y_end = start.y - cosf(angle)*length;
+  Vector2 end = {x_end,y_end};
+
+  float new_length = length*0.7;
+  float new_thickness = thickness*0.7;
+  float new_angle_l = angle + BRANCH_ANGLE;
+  float new_angle_r = angle - BRANCH_ANGLE;
+  DrawLineEx(start,end,thickness,BLACK);
+  DrawBranch(end,new_length,new_angle_l,new_thickness);
+  DrawBranch(end,new_length,new_angle_r,new_thickness);
+}
+
+int main(int argc,char *argv[]) {
+  InitWindow(WIDTH, HEIGHT, "Fractal Tree Visualiser");
+
+    SetTargetFPS(1); // Setting to 1 because only loading the generated tree once each time
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RED);
+            float length = 250;
+            Vector2 start = {WIDTH/2,HEIGHT-20};
+            DrawBranch(start,length,0,20);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
