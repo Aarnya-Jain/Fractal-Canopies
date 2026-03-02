0eb3acf061af2abee1c14ec41049418909a83a0f
Aarnya Jain <aarnyajain417@gmail.com>
2026-03-02 16:18:45 +0530

    Graphics are fun !!!!!

--- L-system.c
+++ L-system.c
@@ -7,32 +7,68 @@
 #define WIDTH 950
 #define HEIGHT 650
 
-// Base Grammar :
+// Base Grammar Used :
 /*
-    F → F[+F][-F]
+    S -> FB
+    F -> FF
+    B -> [lFB][rFB] | FB
 */
 
-void DrawBranch(Vector2 start,float length,float angle,float thickness,float ratio,float branch_angle_l,float branch_angle_r,float wind,float baseThickness) {
-  if(length < 1) return;
+Vector2 DrawF(Vector2 start,float length,float angle,float thickness,float wind,float baseThickness) {
 
-  // wind logic
   float sway = (1.0f - thickness/baseThickness);
   float windOffset = wind*sway;
 
-  float x_end = start.x + sinf(angle+windOffset)*length;
-  float y_end = start.y - cosf(angle+windOffset)*length;
+  float x_mid = start.x + sinf(angle+windOffset)*length;
+  float y_mid = start.y - cosf(angle+windOffset)*length;
+  Vector2 mid = {x_mid,y_mid};
+
+  float x_end = mid.x + sinf(angle+windOffset)*length;
+  float y_end = mid.y - cosf(angle+windOffset)*length;
   Vector2 end = {x_end,y_end};
+
+  DrawLineEx(start,mid,thickness,RAYWHITE);
+  DrawLineEx(mid,end,thickness,RAYWHITE);
+  return end;
+}
+
+void DrawB(Vector2 start,float length,float angle,float thickness,float ratio,float branch_angle_l,float branch_angle_r,float wind,float baseThickness,float branchProb) {
+  if(length < 1) return;
 
   float new_length = length*ratio;
   float new_thickness = thickness*ratio;
-  float new_angle_l = angle + branch_angle_l;
-  float new_angle_r = angle - branch_angle_r;
-  DrawLineEx(start,end,thickness,RAYWHITE);
-  DrawBranch(end,new_length,new_angle_l,new_thickness,ratio,branch_angle_l,branch_angle_r,wind,baseThickness);
-  DrawBranch(end,new_length,new_angle_r,new_thickness,ratio,branch_angle_l,branch_angle_r,wind,baseThickness);
+
+  float r = (float)rand() / RAND_MAX;
+
+  if(r <= branchProb) {
+    // lFB
+    float new_angle_l = angle + branch_angle_l;
+    Vector2 leftEnd = DrawF(start,length,new_angle_l,thickness,wind,baseThickness);
+    DrawB(leftEnd,new_length,new_angle_l,new_thickness,ratio,branch_angle_l,branch_angle_r,wind,baseThickness,branchProb);
+
+    // rFB
+    float new_angle_r = angle - branch_angle_r;
+    Vector2 RightEnd = DrawF(start,length,new_angle_r,thickness,wind,baseThickness);
+    DrawB(RightEnd,new_length,new_angle_r,new_thickness,ratio,branch_angle_l,branch_angle_r,wind,baseThickness,branchProb);
+  } else{
+    Vector2 end = DrawF(start,length,angle,thickness,wind,baseThickness);
+    DrawB(end,new_length,angle,new_thickness,ratio,branch_angle_l,branch_angle_r,wind,baseThickness,branchProb);
+  }
+
+
+}
+
+void DrawTree(Vector2 start,float length,float angle,float thickness,float ratio,float branch_angle_l,float branch_angle_r,float wind,float baseThickness,float branchProb) {
+
+  Vector2 end = DrawF(start,length,angle,thickness,wind,baseThickness);
+
+  float new_length = length*ratio;
+  float new_thickness = thickness*ratio;
+  DrawB(end,new_length,angle,new_thickness,ratio,branch_angle_l,branch_angle_r,wind,baseThickness,branchProb);
 }
 
 int main(int argc,char *argv[]) {
+
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(WIDTH, HEIGHT, "Fractals Visualiser");
 
@@ -59,6 +95,9 @@ int main(int argc,char *argv[]) {
     // wind toggle
     bool wind = false;
 
+    // randomness
+    float branchProbability = 0.7f;
+
     // dropdown params
     int fractalIndex = 0;
     bool fractalEdit = false;
@@ -81,10 +120,10 @@ int main(int argc,char *argv[]) {
                 &symmetric
             );
 
-            GuiLabel((Rectangle){120, l+=20, 200, 20}, "Tree Parameters");
+            GuiLabel((Rectangle){120, l+=20, 200, 20}, "L - Sys Parameters");
 
             GuiSliderBar(
-                (Rectangle){60, l, 200, 20},
+                (Rectangle){60, l+=20, 200, 20},
                 "Length", TextFormat("%.1f", length),
                 &length, 50.0f, 500.0f
             );
@@ -125,11 +164,19 @@ int main(int argc,char *argv[]) {
               );
             }
 
+            GuiSliderBar(
+                (Rectangle){60, l+=30, 200, 20},
+                "Branch %",
+                TextFormat("%.0f%%", branchProbability * 100),
+                &branchProbability,
+                0.0f, 1.0f
+            );
+
             GuiToggle(
                   (Rectangle){60,l+=30,200,20},
                   "Wind",
                   &wind
-              );
+            );
 
               if(GuiDropdownBox(
                   (Rectangle){60,l+=30,200,20},
@@ -151,7 +198,8 @@ int main(int argc,char *argv[]) {
             }
 
             Vector2 start = {w/2,h-20};
-            DrawBranch(start,length,0,thickness,ratio,(angleDegL)*DEG2RAD,(angleDegR)*DEG2RAD,windValue,thickness);
+            srand(42);
+            DrawTree(start,length,0,thickness,ratio,(angleDegL)*DEG2RAD,(angleDegR)*DEG2RAD,windValue,thickness,branchProbability);
         EndDrawing();
     }
 
